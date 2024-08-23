9533158872f7f34866e8060de787fb52766daf9d
CipherStrike-101 <148131867+CipherStrike-101@users.noreply.github.com>
2024-08-23 06:13:44 -0700

    Add files via upload

---
+++ variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#simple bash scripting , just playing around with it 
+
+echo "Hello , there what is your name ? "
+read name
+
+today_date=$(date)
+
+
+echo "Welcome $name to our first hacking conference in South Africa "
+echo "And today's date is $today_date"
+
+
+echo "
+
